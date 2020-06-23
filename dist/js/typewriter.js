d4664620b4b9572c5c96b94ccab89c22efb576ed
Abdus Saboor Gaffari Mohammed <abdussaboorgaffari.mohammed@cognizant.com>
2020-06-23 15:30:10 +0530

    changed animation timing

--- dist/js/typewriter.js
+++ dist/js/typewriter.js
@@ -1,38 +1,38 @@
-const TypeWriter = function(txtElement, words, wait=3000) {
+const TypeWriter = function (txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
     this.words = words;
     this.txt = '';
     this.wordIndex = 0;
-    this.wait = parseInt(wait,10);
+    this.wait = parseInt(wait, 10);
     this.type();
     this.isDeleting = false;
 }
 
-TypeWriter.prototype.type = function() {
+TypeWriter.prototype.type = function () {
 
     const current = this.wordIndex % this.words.length;
     const fullText = this.words[current];
-    let typeSpeed = 300;
+    let typeSpeed = 175;
 
-    if(this.isDeleting) {
-        this.txt = fullText.substring(0,this.txt.length - 1)
+    if (this.isDeleting) {
+        this.txt = fullText.substring(0, this.txt.length - 1)
         typeSpeed /= 2;
     } else {
-        this.txt = fullText.substring(0,this.txt.length + 1)
+        this.txt = fullText.substring(0, this.txt.length + 1)
     }
 
-    if(!this.isDeleting && this.txt === fullText) {
+    if (!this.isDeleting && this.txt === fullText) {
         typeSpeed = this.wait;
         this.isDeleting = true;
-    } else if(this.isDeleting && this.txt === '') {
+    } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.wordIndex++;
-        typeSpeed = 500;
+        typeSpeed = 250;
     }
 
-    this.txtElement.innerHTML = `&nbsp<span class="text-cursor">${ this.txt }</span>_`;
-    
-    setTimeout(() => this.type(),typeSpeed)
+    this.txtElement.innerHTML = `&nbsp<span class="text-cursor">${this.txt}</span>_`;
+
+    setTimeout(() => this.type(), typeSpeed)
 }
 
 document.addEventListener('DOMContentLoaded', init);
@@ -42,5 +42,5 @@ function init() {
     const words = JSON.parse(txtElement.getAttribute('data-words'));
     const wait = txtElement.getAttribute('data-wait');
 
-    new TypeWriter(txtElement,words, wait);
+    new TypeWriter(txtElement, words, wait);
 }
