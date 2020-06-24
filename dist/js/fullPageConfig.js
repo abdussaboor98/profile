8a9902cf68df0605561a1644e5c199020075ab93
Abdus Saboor Gaffari Mohammed <abdussaboorgaffari.mohammed@cognizant.com>
2020-06-24 13:47:11 +0530

    projects and highlights added

--- dist/js/fullPageConfig.js
+++ dist/js/fullPageConfig.js
@@ -7,3 +7,10 @@ const fp = new fullpage('#fullpage', {
     scrollOverflow: true,
     scrollOverflowReset: true,
 });
+
+
+// Additional
+$(".navbar-toggler").click(function () {
+    const icon = document.getElementById("icon");
+    icon.style.transform = icon.style.transform === 'rotate(180deg)' ? '' : 'rotate(180deg)';
+})
